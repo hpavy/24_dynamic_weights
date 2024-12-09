5809720b776d00cf69fe39d2f1400727710ee7db
hugopavy <hugo.pavy@etu.minesparis.psl.eu>
2024-12-09 12:34:31 +0000

    piche

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ print(device)
 
 ############# VARIABLES ################
 
-folder_result_name = "3_modele_3_case_2"  # name of the result folder
+folder_result_name = "11_model_1_case_1_bigger"  # name of the result folder
 
 # Uniquement si nouveau modèle
 
 hyper_param_init = {
-    "H": 230.67,  # la rigidité du ressort
-    "ya0": 0.0175,  # la position initiale du ressort
+    "H": 261.394,  # la rigidité du ressort
+    "ya0": 0.01,  # la position initiale du ressort
     "m": 1.57,    # la masse du ressort
-    "file": "data_john_3_case_2.csv",
+    "file": "data_john_moving_1.csv",
     "nb_epoch": 10000,  # epoch number
     "save_rate": 50,  # rate to save
     "dynamic_weights": True,
@@ -32,10 +32,10 @@ hyper_param_init = {
     "n_pde_test": 5000,
     "n_data_test": 5000,
     "nb_points_axes": 12,  # le nombre de points pris par axe par pas de temps
-    "x_min": -0.03,
-    "x_max": 0.03,
-    "y_min": -0.03,
-    "y_max": 0.03,
+    "x_min": -0.1,
+    "x_max": 0.1,
+    "y_min": -0.06,
+    "y_max": 0.06,
     "t_min": 6.5,
     "t_max": 8,
     "nb_points_close_cylinder": 50,
